51404ca54b7896021934e0b2644f04c3855d2a32
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-03-18 20:36:42 +0000

    crear carta

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    function palo= ['PICA', 'CORAZON', 'TREBOL', ''DIAMANTE];
+      let paloAleatorio = palo[Math.floor(Math.random() * palo.length)];
+
+    function numero= ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+    function generarExcusa() {
+       
+        let numeroAleatorio = numero[Math.floor(Math.random() * numero.length)];
+        let cartaCompleta= paloAleatorio + ' ' + numeroAleatorio + ' ' + paloAleatorio';
+        return cartaCompleta;
+    }
+    let elementoCartaCompleta = document.getElementById('cartaCompleta');
+    elementoCartaCompleta.innerHTML = generarCarta();
+}
+  console.log();
 };
